6fd768359d270d2e7ea97e6a92ad58a6c9228b97
MaTHtoE <canyara@gmail.com>
2018-10-05 18:13:14 +0800

    修改bugly依赖

---
+++ scripts/postlink.js
@@ -0,0 +1,106 @@
+var fs = require("fs");
+var glob = require("glob");
+var inquirer = require("inquirer");
+var xcode = require("xcode");
+var path = require("path");
+var plist = require("plist");
+var _ = require("lodash");
+var pbxFile = require("xcode/lib/pbxFile");
+var package = require("../../../../package.json");
+
+var ignoreNodeModules = { ignore: "node_modules/**" };
+var appDelegatePaths = glob.sync("**/AppDelegate.m", ignoreNodeModules);
+
+// Fix for https://github.com/Microsoft/react-native-code-push/issues/477
+// Typical location of AppDelegate.m for newer RN versions: $PROJECT_ROOT/ios/<project_name>/AppDelegate.m
+// Let's try to find that path by filtering the whole array for any path containing <project_name>
+// If we can't find it there, play dumb and pray it is the first path we find.
+var appDelegatePath =
+  findFileByAppName(appDelegatePaths, package ? package.name : null) ||
+  appDelegatePaths[0];
+var plistPath = glob.sync(
+  path.join(path.dirname(appDelegatePath), "*Info.plist").replace(/\\/g, "/"),
+  ignoreNodeModules
+)[0];
+// var appDelegateContents = fs.readFileSync(appDelegatePath, "utf8");
+var plistContents = fs.readFileSync(plistPath, "utf8");
+
+addFrameworkAndSearchPath();
+
+// Helper that filters an array with AppDelegate.m paths for a path with the app name inside it
+// Should cover nearly all cases
+function findFileByAppName(array, appName) {
+  if (array.length === 0 || !appName) return null;
+  for (var i = 0; i < array.length; i++) {
+    var path = array[i];
+    if (path && path.indexOf(appName) !== -1) {
+      return path;
+    }
+  }
+  return null;
+}
+
+function addFrameworkAndSearchPath() {
+  var projectPath = glob.sync("**/project.pbxproj", ignoreNodeModules)[0];
+  var project = xcode.project(projectPath);
+  var frameworkPath = path.join(
+    __dirname,
+    "../node_modules/react-native-bugly/RNBugly/SDK/Bugly.framework"
+  );
+  var project_dir = path.join(__dirname);
+  var project_relative = path.relative(project_dir, frameworkPath);
+  project.parse(function(error) {
+    if (error) {
+      console.log("xcode project error is", error);
+    } else {
+      var target = project.getFirstTarget().uuid;
+      var file = new pbxFile(project_relative, {
+        customFramework: true,
+        target: target
+      });
+      file.uuid = project.generateUuid();
+      file.fileRef = project.generateUuid();
+      file.target = target;
+      if (project.hasFile(file.path)) return false;
+      project.addToPbxBuildFileSection(file); // PBXBuildFile
+      project.addToPbxFileReferenceSection(file); // PBXFileReference
+      project.addToFrameworksPbxGroup(file); // PBXGroup
+      project.addToPbxFrameworksBuildPhase(file); // PBXFrameworksBuildPhase
+      //project.addToFrameworkSearchPaths(file);
+      addSearchPaths(
+        project,
+        '"$(SRCROOT)/../node_modules/react-native-bugly/RNBugly/SDK/**"'
+      );
+      fs.writeFileSync(projectPath, project.writeSync());
+    }
+  });
+}
+
+function addSearchPaths(project, frameworkSearchPath) {
+  const config = project.pbxXCBuildConfigurationSection();
+  const INHERITED = '"$(inherited)"';
+  Object.keys(config)
+    .filter(ref => ref.indexOf("_comment") === -1)
+    .forEach(ref => {
+      const buildSettings = config[ref].buildSettings;
+      const shouldVisitBuildSettings =
+        buildSettings["PRODUCT_NAME"] === package.name;
+      if (shouldVisitBuildSettings) {
+        if (
+          !buildSettings["FRAMEWORK_SEARCH_PATHS"] ||
+          buildSettings["FRAMEWORK_SEARCH_PATHS"] === INHERITED
+        ) {
+          buildSettings["FRAMEWORK_SEARCH_PATHS"] = [INHERITED];
+        }
+        var framworkIndex = _.findIndex(
+          buildSettings["FRAMEWORK_SEARCH_PATHS"],
+          function(path) {
+            return path == frameworkSearchPath;
+          }
+        );
+        if (framworkIndex === -1) {
+          buildSettings["FRAMEWORK_SEARCH_PATHS"].push(frameworkSearchPath);
+        }
+      }
+    });
+}
---
+++ scripts/postunlink.js
@@ -0,0 +1,94 @@
+var fs = require("fs");
+var glob = require("glob");
+var inquirer = require("inquirer");
+var path = require("path");
+var plist = require("plist");
+var xcode = require("xcode");
+var _ = require("lodash");
+var pbxFile = require("xcode/lib/pbxFile");
+var package = require("../../../../package.json");
+
+var ignoreNodeModules = { ignore: "node_modules/**" };
+var appDelegatePaths = glob.sync("**/AppDelegate.m", ignoreNodeModules);
+
+// Fix for https://github.com/Microsoft/react-native-code-push/issues/477
+// Typical location of AppDelegate.m for newer RN versions: $PROJECT_ROOT/ios/<project_name>/AppDelegate.m
+// Let's try to find that path by filtering the whole array for any path containing <project_name>
+// If we can't find it there, play dumb and pray it is the first path we find.
+var appDelegatePath =
+  findFileByAppName(appDelegatePaths, package ? package.name : null) ||
+  appDelegatePaths[0];
+var plistPath = glob.sync(
+  path.join(path.dirname(appDelegatePath), "*Info.plist").replace(/\\/g, "/"),
+  ignoreNodeModules
+)[0];
+
+removeFrameworkAndSearchPath();
+
+// Helper that filters an array with AppDelegate.m paths for a path with the app name inside it
+// Should cover nearly all cases
+function findFileByAppName(array, appName) {
+  if (array.length === 0 || !appName) return null;
+  for (var i = 0; i < array.length; i++) {
+    var path = array[i];
+    if (path && path.indexOf(appName) !== -1) {
+      return path;
+    }
+  }
+  return null;
+}
+
+function removeFrameworkAndSearchPath() {
+  var projectPath = glob.sync("**/project.pbxproj", ignoreNodeModules)[0];
+  var project = xcode.project(projectPath);
+  var frameworkPath = path.join(
+    __dirname,
+    "../node_modules/react-native-bugly/RNBugly/SDK/Bugly.framework"
+  );
+  var project_dir = path.join(__dirname);
+  var project_relative = path.relative(project_dir, frameworkPath);
+  project.parse(function(error) {
+    if (error) {
+      console.log("xcode project error is", error);
+    } else {
+      var target = project.getFirstTarget().uuid;
+      var file = new pbxFile(project_relative, {
+        customFramework: true,
+        target: target
+      });
+      file.target = target;
+      project.removeFromPbxBuildFileSection(file); // PBXBuildFile
+      project.removeFromPbxFileReferenceSection(file); // PBXFileReference
+      project.removeFromFrameworksPbxGroup(file); // PBXGroup
+      project.removeFromPbxFrameworksBuildPhase(file); // PBXFrameworksBuildPhase
+      //project.removeFromFrameworkSearchPaths(file);
+      removeSearchPaths(
+        project,
+        '"$(SRCROOT)/../node_modules/react-native-bugly/RNBugly/SDK/**"'
+      );
+      fs.writeFileSync(projectPath, project.writeSync());
+    }
+  });
+}
+
+function removeSearchPaths(project, frameworkSearchPath) {
+  const config = project.pbxXCBuildConfigurationSection();
+  Object.keys(config)
+    .filter(ref => ref.indexOf("_comment") === -1)
+    .forEach(ref => {
+      const buildSettings = config[ref].buildSettings;
+      const shouldVisitBuildSettings =
+        buildSettings["PRODUCT_NAME"] === package.name;
+      if (shouldVisitBuildSettings) {
+        if (buildSettings["FRAMEWORK_SEARCH_PATHS"]) {
+          const paths = _.remove(
+            buildSettings["FRAMEWORK_SEARCH_PATHS"],
+            function(path) {
+              return path !== frameworkSearchPath;
+            }
+          );
+          buildSettings["FRAMEWORK_SEARCH_PATHS"] = paths;
+        }
+      }
+    });
+}
